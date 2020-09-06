228e50ed6a8bde50d636fb168f90085081993a62
AbdyT <tabyldyev34@gmail.com>
2020-09-06 16:32:33 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+console.log('arr' + ' - object');
+// console.log(4 + +"5"); //унарный плюс
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(--decr);
+
+console.log(5%2);
+
+
+
+
+
+
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = "Alex";
+ 
+// const bool = false;
+
+// // console.log(something);
+
+// let und;
+
+// // console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.age);
+// obj.name = "MIKE";
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 1, obj, {}, [], 'apple.bmp'];
+// console.log(arr[1]);
+
